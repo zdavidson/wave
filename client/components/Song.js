65b8e73b576c2584bbf8c844d7d7c235113099d2
Zoë Davidson <zoe.a.h.davidson@gmail.com>
2021-06-27 16:54:50 -0400

    stateless solution for highlighting current song

--- client/components/Song.js
+++ client/components/Song.js
@@ -1,23 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const audio = document.createElement("audio");
-
 const startAudio = (audioUrl) => {
   audio.src = audioUrl;
   audio.load();
   audio.play();
 };
 
+const makeActive = (songId) => {
+  document.getElementById(songId).classList.add("active");
+};
+
+const makeInactive = (songId) => {
+  if (songId !== 0) {
+    document.getElementById(songId).classList.remove("active");
+  } else {
+    console.log("no previous song");
+  }
+};
+
 export default function Song(props) {
+  let [currentSong, setCurrentSong] = useState({});
+  let formerSongId;
+
   const artistName = props.data.artist.name;
+
   return (
     <>
       {props.data.songs.map((song) => {
         return (
-          <tr key={song.id}>
+          <tr key={song.id} id={song.id}>
             <td>
               <i
                 onClick={() => {
+                  formerSongId = currentSong.id || 0;
+                  makeInactive(formerSongId);
+                  currentSong = song;
+                  makeActive(currentSong.id);
                   startAudio(song.audioUrl);
                 }}
                 className="fa fa-play-circle"
